cccd6007b06b153963a1292fe7d49072c56d3d7f
Otavio <otavio.silva@symphony.com>
2020-01-13 07:48:11 -0300

    Fixed the project maven build

---
+++ my-app/src/main/java/com/osilva/devpath/App.java
@@ -0,0 +1,13 @@
+package com.osilva.devpath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ my-app/src/test/java/com/osilva/devpath/AppTest.java
@@ -0,0 +1,20 @@
+package com.osilva.devpath;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
