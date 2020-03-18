c5314017c764d465ddf0d1d69fc6d654b9a20c6f
Otavio Silva <otavioguido92@gmail.com>
2020-03-18 17:45:07 -0300

    Mudei herança do spring para uma dependencia e adicionei a herança do projeto

--- dataBase/src/test/java/com/osilva/dataBase/DataBaseApplicationTests.java
+++ dataBase/src/test/java/com/osilva/dataBase/DataBaseApplicationTests.java
@@ -1,15 +1,5 @@
 package com.osilva.dataBase;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-
-import com.osilva.dataBase.models.User;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /*@RunWith(SpringRunner.class)
 @SpringBootTest*/
