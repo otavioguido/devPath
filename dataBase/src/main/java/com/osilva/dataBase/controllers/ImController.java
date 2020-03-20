b30aeb9ae7f8f3c75362cddb48a34e1278cdb139
Otavio Silva <otavioguido92@gmail.com>
2020-03-20 14:10:32 -0300

    Fixed issue in IM controller

--- dataBase/src/main/java/com/osilva/dataBase/controllers/ImController.java
+++ dataBase/src/main/java/com/osilva/dataBase/controllers/ImController.java
@@ -17,17 +17,17 @@ public class ImController {
 
   @PostMapping("/createIM")
   public Long createIM(@RequestBody ObjectNode json){
-    User user1 = searchUserById((Long) json.get("user1").asLong());
-    User user2 = searchUserById((Long) json.get("user2").asLong());
+    /*User user1 = searchUserById((Long) json.get("user1").asLong());
+    User user2 = searchUserById((Long) json.get("user2").asLong());*/
 
     IM im = new IM();
-    im.getUsers().add(Long.toString(user1.getId()));
-    im.getUsers().add(Long.toString(user2.getId()));
+    /*im.getUsers().add(Long.toString(user1.getId()));
+    im.getUsers().add(Long.toString(user2.getId()));*/
     imService.saveIm(im);
-    user1.getIms().add(Long.toString(im.getId()));
+    /*user1.getIms().add(Long.toString(im.getId()));
     user2.getIms().add(Long.toString(im.getId()));
     imService.saveUser(user1);
-    imService.saveUser(user2);
+    imService.saveUser(user2);*/
     return im.getId();
   }
 
